db2fb3fec0ec6bfc4728c5b26f0fdb454cd1f552
simpledipmle <87315448+simpledipmle@users.noreply.github.com>
2021-07-12 15:53:17 +0300

    Add files via upload

---
+++ simplebot.py
@@ -0,0 +1,1 @@
+import telebot bot = telebot.TeleBot("1744345981:AAG-XxYYOBAitNhG4NAaTWngcme_Q57J2S8") @bot.message_handler(commands=['help', 'start']) def send_welcome(message): msg = bot.send_message(message.chat.id, 'Привет! Я codex_bot!') @bot.message_handler(commands=['auth']) def send_auth(message): pass bot.polling()
